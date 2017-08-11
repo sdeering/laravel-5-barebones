5cc0a168a682cb8b911b5b67dbf272a3c918501a
sdeering <samdeering@gmail.com>
2017-08-11 11:34:02 +1000

    init

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin/dashboard');
+    }
+}
